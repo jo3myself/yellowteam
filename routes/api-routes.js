0b69cab77f017a19db32e7055f404a29255aec50
mariomelchor <mario@mariomelchor.com>
2017-10-01 16:20:54 -0700

    Added route for updating user

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
   });
 
   // Add a New user
-  app.post("/user/new", function(req, res) {
+  app.post("/user", function(req, res) {
     // console.log("User Data:");
     // console.log(req.body);
     const password = req.body.password;
@@ -34,7 +34,31 @@ module.exports = function(app) {
         userName: req.body.user_name,
         password: hash,
         profileImage: req.body.profile_image,
-        location: req.body.location,
+        location: req.body.location
+      }).then(function(dbUser) {
+        res.json(dbUser);
+      });
+    });
+  });
+
+  // PUT route for Updating User.
+  app.put("/user", function(req, res) {
+    const password = req.body.password;
+
+    // Hash the password then save to DB
+    bcrypt.hash(password, saltRounds).then(function(hash) {
+      db.User.update({
+        name: req.body.first_name,
+        email: req.body.email,
+        phone: req.body.phone_number,
+        userName: req.body.user_name,
+        password: hash,
+        profileImage: req.body.profile_image,
+        location: req.body.location
+      }, {
+        where: {
+          id: req.body.id
+        }
       }).then(function(dbUser) {
         res.json(dbUser);
       });
