00dd3e1e8826848bc4a47d50b12be48e3e78feb4
JakeGreer <Jakegreer11@gmail.com>
2017-10-03 15:08:40 -0700

    added thumbnail slider for product view

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -7,5 +7,84 @@
   });
 
   // Activates Carousel
-  $('.carousel').carousel();
+	$('.carousel').carousel();
+	
+	$('#myCarousel').carousel({
+    interval: 4000
+});
+
+// handles the carousel thumbnails
+$('[id^=carousel-selector-]').click( function(){
+  var id_selector = $(this).attr("id");
+  var id = id_selector.substr(id_selector.length -1);
+  id = parseInt(id);
+  $('#myCarousel').carousel(id);
+  $('[id^=carousel-selector-]').removeClass('selected');
+  $(this).addClass('selected');
+});
+
+// when the carousel slides, auto update
+$('#myCarousel').on('slid', function (e) {
+  var id = $('.item.active').data('slide-number');
+  id = parseInt(id);
+  $('[id^=carousel-selector-]').removeClass('selected');
+  $('[id=carousel-selector-'+id+']').addClass('selected');
+});
+
+
+$.get("/api/products", function(data) {
+	var categories=[];
+	var category = [];
+	for (let i = 0; i < data.length; i++) {
+		categories.push(data[i].category); 
+	}
+    $.each(categories, function(i, elm){
+        if($.inArray(elm, category) === -1) category.push(elm);
+    });
+    console.log(category);
+	for (var l = 0; l < category.length; l++) {
+		var cat = $('<a class="dropdown-item" id="dropDownCategory" name="category" href="/?category_search=' + category[l] + '">' + category[l] + '</a>');
+		$(".searchDropDown").append(cat);
+	}
+});
+
+var url = window.location.search;
+var ProductCategory;
+if (url.indexOf("?category_search=") !== -1) {
+	ProductCategory = url.split("=")[1];
+	dropdownList(ProductCategory);
+}
+else {
+	dropdownList();
+}
+
+
+function dropdownList(cat) {
+    ProductCategory = cat || "";
+    if (ProductCategory) {
+      ProductCategory = "/?category_search=" + ProductCategory;
+    }
+    $.get("/api/products" + ProductCategory, function(data) {
+    	console.log(data);
+      	// for (var i = 0; i < data.length; i++) {
+      	// 	$("#test1").append("<p>" + data[i].productName + "</p>");
+      	// }    
+    });
+  }
+
+$("#searchButton").click(function() {
+	event.preventDefault();
+	var searchProduct = $("#searchInput").val();
+	$.get("/api/search/" + searchProduct, function(data) {
+		console.log(data);
+		// for (var i = 0; i < data.length; i++) {
+		// 	$("#test1").append('<div class="col-sm-12 col-xl-5"><div class="media media-category"> <img class="d-flex align-self-center mr-3" src="http:' + data[i].imageURL + '" alt="Generic placeholder image"><div class="media-body"><h5 class="mt-0">' + data[i].productName + '</h5><p>Category: ' + data[i].category + '</p> <p>Price: ' + data[i].price + '</p> <a href="/' + data[i].id + '">More Info &rarr;</a></div></div>');  	
+		// }
+	});
+});
+
+
+
+
+
 });
