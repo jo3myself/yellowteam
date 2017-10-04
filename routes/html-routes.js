82a28394cd3d454e72e32cd84b22448a1d6015f3
CODING <CODING>
2017-10-04 11:01:34 -0700

    update files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -54,6 +54,42 @@ module.exports = function(app) {
     res.render('addProducts', {});
   });
 
+// search for products with this userId and pass it to handlebars
+  app.get("/users/:id", function(req, res) {
+    db.Product.findAll({
+      where: {
+        UserId: req.params.id
+      },
+      include: [db.User]
+    }).then(function(results) {
+      res.render("test", { product: results });
+    });
+  });
+
+// search for product with this Id and pass it to handlebars
+  app.get("/product/:id", function(req, res) {
+    db.Product.findOne({
+      where: {
+        Id: req.params.id
+      },
+      include: [db.User]
+    }).then(function(results) {
+      res.render("testview", { product: results });
+    });
+  });
+
+// search for product category and pass it to handlebars
+  app.get("/category/:category", function(req, res) {
+    db.Product.findAll({
+      where: {
+        category: req.params.category
+      },
+      include: [db.User]
+    }).then(function(results) {
+      res.render("search", { productsSearched: results });
+    });
+  });
+
 
 };
 
