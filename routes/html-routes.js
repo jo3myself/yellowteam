39075d2eb578184a0f0976b124e62a5cffe49051
Brendan Ha <ha.brendan07@gmail.com>
2017-10-04 18:58:33 -0700

    updates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,6 +25,14 @@ module.exports = function(app) {
     res.render('addProducts', {});
   });
 
+  app.get("/store", function(req, res) {
+       res.render('store', {});
+    });
+  
+   app.get("/product-view", function(req, res) {
+      res.render('product-view', {});
+    });
+
   
   // do the search and pass the data to search handlebars
   app.get("/search/:search", function(req, res) {
@@ -44,3 +52,4 @@ module.exports = function(app) {
     };
   });
 
+};
