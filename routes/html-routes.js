30a7e9701a3499906aae2ebd84f64c349c90dcea
Brendan Ha <ha.brendan07@gmail.com>
2017-10-04 18:43:27 -0700

    updates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -21,22 +21,10 @@ module.exports = function(app) {
     res.render('user', {});
   });
 
-<<<<<<< HEAD
   app.get('/addProducts' , function (req, res) {
     res.render('addProducts', {});
   });
 
-=======
-
-  app.get("/store", function(req, res) {
-    res.render('store', {});
-  });
-
-  app.get("/product-view", function(req, res) {
-    res.render('product-view', {});
-  })
-
->>>>>>> d39b6ba9189eec532d994744481d20cd46ae4ded
   
   // do the search and pass the data to search handlebars
   app.get("/search/:search", function(req, res) {
@@ -55,49 +43,4 @@ module.exports = function(app) {
       });
     };
   });
-
-
-
-<<<<<<< HEAD
-
-=======
-// search for products with this userId and pass it to handlebars
-  app.get("/users/:id", function(req, res) {
-    db.Product.findAll({
-      where: {
-        UserId: req.params.id
-      },
-      include: [db.User]
-    }).then(function(results) {
-      res.render("test", { product: results });
-    });
-  });
-
-// search for product with this Id and pass it to handlebars
-  app.get("/product/:id", function(req, res) {
-    db.Product.findOne({
-      where: {
-        Id: req.params.id
-      },
-      include: [db.User]
-    }).then(function(results) {
-      res.render("testview", { product: results });
-    });
-  });
-
-// search for product category and pass it to handlebars
-  app.get("/category/:category", function(req, res) {
-    db.Product.findAll({
-      where: {
-        category: req.params.category
-      },
-      include: [db.User]
-    }).then(function(results) {
-      res.render("search", { productsSearched: results });
-    });
-  });
-
-
-};
->>>>>>> d39b6ba9189eec532d994744481d20cd46ae4ded
 
