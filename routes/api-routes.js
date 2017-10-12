912d3e784a2a9ad7c191a0f7c37f629bb5705e44
Jake Greer <jakegreer11@gmail.com>
2017-10-12 16:06:40 -0700

    branch currently not up to date

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,182 +9,199 @@
 var db = require("../models");
 var bCrypt = require('bcrypt-nodejs');
 var formidable = require('formidable');
-var path = require('path');  
+var path = require('path');
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-  // get all products
-  app.get("/api/products/all", function(req, res) {
-    db.Product.findAll({}).then(function(results) {
-      res.json(results);
-    });
-  });
-
-  // get by category
-  app.get("/api/category/:category", function(req, res) {
-    db.Product.findAll({
-        where: {
-         category: req.params.category
-        }
-    }).then(function(results) {
-      res.json(results);
-    });
-  });
-
-  // Add a New Products
-  app.post("/addProducts", function(req, res) {
-    // Setup formidable
-    var form = new formidable.IncomingForm();
-
-    // Parse the form request
-    form.parse(req, function(err, fields, files) {
-      // Add Product to DB    
-      db.Product.create({
-        UserId: fields.userID,
-        productName: fields.productName,
-        category: fields.category,
-        price: fields.price,
-        description: fields.description,
-        imageURL: files.imageURL.name
-      }).then(function(dbProduct) {
-        res.redirect('/product/' + dbProduct.id );
-      });
-    });
-
-    form.on('fileBegin', function (name, file){
-      file.path = path.basename(path.dirname('../')) + '/public/uploads/products/' + file.name;     
-    });
-
-    form.on('end', function() {
-      console.log('Thanks File Uploaded');
-    });
-  });
-
-  // PUT route for Updating User.
-  app.put("/user", function(req, res) {
-    
-    var password = req.body.password;
-
-    db.User.findOne({
-      where: {
-        id: req.body.id
-      }
-    }).then(function(dbUser) {
-      // Checks if password input matches hash in DB
-      if( password !== dbUser.password ) {
-        // Hash the password then save to DB
-        password = bCrypt.hashSync(password);
-      }
-
-      db.User.update({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone_number,
-        userName: req.body.user_name,
-        password: password,
-        location: req.body.location
-      }, {
-        where: {
-          id: req.body.id
-        }
-      }).then(function(dbUser) {
-        res.json(dbUser);
-      });
-
-    });
-
-  });
-
-  // PUT route for updating products
-  app.put('/editProducts/:id', function(req, res) {
-    if(req.isAuthenticated()) {
-      // Setup formidable
-      var form = new formidable.IncomingForm();
-
-      // Parse the form request
-      form.parse(req, function(err, fields, files) {
-        db.Product.update({
-          productName: fields.edited_product_name,
-          price: fields.edited_price,
-          category: fields.edited_category,
-          description: fields.edited_description,
-          imageURL: files.edited_imageURL.name
-        }, {
-          where: {
-            Id: req.params.id
-          }
-        }).then(function(results) {
-          console.log(req.params.id);
-          res.redirect('/editProducts/' + req.params.id)
+    // get all products
+    app.get("/api/products/all", function(req, res) {
+        db.Product.findAll({}).then(function(results) {
+            res.json(results);
         });
-      });
-
-      form.on('fileBegin', function (name, file){
-        file.path = path.basename(path.dirname('../')) + '/public/uploads/products/' + file.name;     
-      });
-  
-      form.on('end', function() {
-        console.log('Thanks File Uploaded');
-      });
-
-    }
-  });
-  
-  app.delete("/api/products/:id", function(req, res) {
-    db.Product.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(results) {
-      res.json(results);
-      // res.render("userProducts", { userProducts: data });
     });
-  });
 
-  app.get("/api/login", function(req, res) {
-    db.User.findOne({
-      where: {
-        $and: [
-          {userName: req.params.userName },
-          {password: req.params.password }
-        ]
-      }
-    }).then(function(results) {
-      res.json(results);
-    }).catch(function (err) {
-      res.json("");
-    }); 
-  });
+    // get by category
+    app.get("/api/category/:category", function(req, res) {
+        db.Product.findAll({
+            where: {
+                category: req.params.category
+            }
+        }).then(function(results) {
+            res.json(results);
+        });
+    });
 
-  // Add a profile image
-  app.put("/profile-image", function(req, res) {
-    // Setup formidable
-    var form = new formidable.IncomingForm();
+    // Add a New Products
+    app.post("/addProducts", function(req, res) {
+        // Setup formidable
+        var form = new formidable.IncomingForm();
 
-    // Parse the form request
-    form.parse(req, function(err, fields, files) {
+        // Parse the form request
+        form.parse(req, function(err, fields, files) {
+            // Add Product to DB    
+            db.Product.create({
+                UserId: fields.userID,
+                productName: fields.productName,
+                category: fields.category,
+                price: fields.price,
+                description: fields.description,
+                imageURL: files.imageURL.name
+            }).then(function(dbProduct) {
+                res.redirect('/product/' + dbProduct.id);
+            });
+        });
 
-      // Add profile image to DB    
-      db.User.update({
-        profileImage: files.profile_image.name
-      }, {
-        where: {
-          id: fields.id
+        form.on('fileBegin', function(name, file) {
+            file.path = path.basename(path.dirname('../')) + '/public/uploads/products/' + file.name;
+        });
+
+        form.on('end', function() {
+            console.log('Thanks File Uploaded');
+        });
+    });
+
+    // PUT route for Updating User.
+    app.put("/user", function(req, res) {
+
+        var password = req.body.password;
+
+        db.User.findOne({
+            where: {
+                id: req.body.id
+            }
+        }).then(function(dbUser) {
+            // Checks if password input matches hash in DB
+            if (password !== dbUser.password) {
+                // Hash the password then save to DB
+                password = bCrypt.hashSync(password);
+            }
+
+            db.User.update({
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone_number,
+                userName: req.body.user_name,
+                password: password,
+                location: req.body.location
+            }, {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function(dbUser) {
+                res.json(dbUser);
+            });
+
+        });
+
+    });
+
+    // PUT route for updating products
+    app.put('/editProducts/:id', function(req, res) {
+        if (req.isAuthenticated()) {
+            // Setup formidable
+            var form = new formidable.IncomingForm();
+
+            // Parse the form request
+            form.parse(req, function(err, fields, files) {
+                db.Product.update({
+                    productName: fields.edited_product_name,
+                    price: fields.edited_price,
+                    category: fields.edited_category,
+                    description: fields.edited_description,
+                    imageURL: files.edited_imageURL.name
+                }, {
+                    where: {
+                        Id: req.params.id
+                    }
+                }).then(function(results) {
+                    console.log(req.params.id);
+                    res.redirect('/editProducts/' + req.params.id)
+                });
+            });
+
+            form.on('fileBegin', function(name, file) {
+                file.path = path.basename(path.dirname('../')) + '/public/uploads/products/' + file.name;
+            });
+
+            form.on('end', function() {
+                console.log('Thanks File Uploaded');
+            });
+
         }
-      }).then(function(dbUser) {  
-        res.redirect('/store/' + req.user.userName );
-      });
     });
 
-    form.on('fileBegin', function (name, file){
-      file.path = path.basename(path.dirname('../')) + '/public/uploads/users/' + file.name;     
+    app.delete("/api/products/:id", function(req, res) {
+        db.Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(results) {
+            res.json(results);
+            // res.render("userProducts", { userProducts: data });
+        });
     });
 
-    form.on('end', function() {
-      console.log('Thanks File Uploaded');
+    app.delete("/products/:id", function(req, res) {
+        db.Product.destroy({
+            where: {
+                id: req.params.id
+            },
+        }).then(function(results) {
+            db.User.findOne({
+                where: {
+                    id: req.params.id
+                }
+            }).then(function(user) {
+                console.log("RESULTSSSSS\n\n\n", user)
+                res.redirect("/store/" + user.name);
+            })
+        })
+    })
+
+    app.get("/api/login", function(req, res) {
+        db.User.findOne({
+            where: {
+                $and: [
+                    { userName: req.params.userName },
+                    { password: req.params.password }
+                ]
+            }
+        }).then(function(results) {
+            res.json(results);
+        }).catch(function(err) {
+            res.json("");
+        });
     });
-  });
+
+    // Add a profile image
+    app.put("/profile-image", function(req, res) {
+        // Setup formidable
+        var form = new formidable.IncomingForm();
+
+        // Parse the form request
+        form.parse(req, function(err, fields, files) {
+
+            // Add profile image to DB    
+            db.User.update({
+                profileImage: files.profile_image.name
+            }, {
+                where: {
+                    id: fields.id
+                }
+            }).then(function(dbUser) {
+                res.redirect('/store/' + req.user.userName);
+            });
+        });
+
+        form.on('fileBegin', function(name, file) {
+            file.path = path.basename(path.dirname('../')) + '/public/uploads/users/' + file.name;
+        });
+
+        form.on('end', function() {
+            console.log('Thanks File Uploaded');
+        });
+    });
 
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,88 +10,88 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads .html
-  app.get("/", function(req, res) {
-    // console.log(req);
-    db.Product.findAll({
-      limit: 4,
-      where: {
-       category: 'Smartphones'
-      },
-      include: [db.User]
-    }).then(function(results) {
-      res.render('index', { product: results} );
+    // index route loads .html
+    app.get("/", function(req, res) {
+        // console.log(req);
+        db.Product.findAll({
+            limit: 4,
+            where: {
+                category: 'Smartphones'
+            },
+            include: [db.User]
+        }).then(function(results) {
+            res.render('index', { product: results });
+        });
     });
-  });
 
 
-  // search for stores by the store username and populate the store page with the results
-  app.get("/store/:store", function(req, res) {
-    console.log(req.isAuthenticated());
-    if (req.params.store) {
-      db.User.findOne({
-        where: {userName: req.params.store},
-        include: [
-            db.Product
-        ]
-      }).then(function(results) {
-         res.render('store', {userInfo: results});
-      });
-    }
-  });
-
-  app.get("/product-view", function(req, res) {
-    res.render('product-view', {});
-  });
-
-  app.get("/store", function(req, res) {
-    res.render('store', {});
-  });
-
-  
-  
-  // do the search and pass the data to search handlebars
-  app.get("/search/:search", function(req, res) {
-    if (req.params.search) {
-      db.Product.findAll({
-        where: {
-          $or: [
-            {productName: { like: '%' + req.params.search + '%' } },
-            {category: { like: '%' + req.params.search + '%' } },
-            {description: { like: '%' + req.params.search + '%' } }
-          ]
-        },
-        include: [db.User]
-      }).then(function(results) {
-        res.render("search", { productsSearched: results });
-      });
-    };
-  });
-
-  // search for product with this Id and pass it to handlebars
-  app.get("/product/:id", function(req, res) {
-    db.Product.findOne({
-      where: {
-        Id: req.params.id
-      },
-      include: [db.User]
-    }).then(function(results) {
-      res.render("product-view", { product: results });
+    // search for stores by the store username and populate the store page with the results
+    app.get("/store/:store", function(req, res) {
+        console.log(req.isAuthenticated());
+        if (req.params.store) {
+            db.User.findOne({
+                where: { userName: req.params.store },
+                include: [
+                    db.Product
+                ]
+            }).then(function(results) {
+                res.render('store', { userInfo: results });
+            });
+        }
     });
-  });
 
-  // search for product category and pass it to handlebars
-  app.get("/category/:category", function(req, res) {
-    db.Product.findAll({
-      where: {
-        category: req.params.category
-      },
-      include: [db.User]
-    }).then(function(results) {
-      res.render("search", { productsSearched: results });
+    app.get("/product-view", function(req, res) {
+        res.render('product-view', {});
     });
-  });
-   
+
+    app.get("/store", function(req, res) {
+        res.render('store', {});
+    });
+
+
+
+    // do the search and pass the data to search handlebars
+    app.get("/search/:search", function(req, res) {
+        if (req.params.search) {
+            db.Product.findAll({
+                where: {
+                    $or: [
+                        { productName: { like: '%' + req.params.search + '%' } },
+                        { category: { like: '%' + req.params.search + '%' } },
+                        { description: { like: '%' + req.params.search + '%' } }
+                    ]
+                },
+                include: [db.User]
+            }).then(function(results) {
+                res.render("search", { productsSearched: results });
+            });
+        };
+    });
+
+    // search for product with this Id and pass it to handlebars
+    app.get("/product/:id", function(req, res) {
+        db.Product.findOne({
+            where: {
+                Id: req.params.id
+            },
+            include: [db.User]
+        }).then(function(results) {
+            res.render("product-view", { product: results });
+        });
+    });
+
+    // search for product category and pass it to handlebars
+    app.get("/category/:category", function(req, res) {
+        db.Product.findAll({
+            where: {
+                category: req.params.category
+            },
+            include: [db.User]
+        }).then(function(results) {
+            res.render("search", { productsSearched: results });
+        });
+    });
+
 };
